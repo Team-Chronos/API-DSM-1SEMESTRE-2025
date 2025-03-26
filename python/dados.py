7a7f45809adf838625370ae1b22623efe9913775
EnzoGabrielCode <enzokof2005@gmail.com>
2025-03-26 13:41:22 -0300

    adicionando a variavel de importacao no arquivo python

--- python/dados.py
+++ python/dados.py
@@ -15,8 +15,9 @@ sh4_df = pd.read_csv(sh4, sep=";", encoding="latin1")
 mun_df = mun_df.rename(columns={"CO_MUN_GEO": "CO_MUN"})
 sh4_df = sh4_df.rename(columns={"CO_SH4": "SH4", "NO_SH4_POR": "PRODUTO"})
 ex24_df = ex24_df.merge(mun_df, on=["CO_MUN"], how="left")
+imp24_df = imp24_df.merge(mun_df, on=["CO_MUN"], how="left")
 
-# Criando variáveis
+# Criando variáveis de exportação
 ex_mes_24 = ex24_df['CO_MES']
 ex_sh4_24 = ex24_df['SH4']
 ex_estado_24 = ex24_df['SG_UF_MUN']
@@ -24,69 +25,138 @@ ex_municipio_24 = ex24_df['CO_MUN']
 ex_liquido_24 = ex24_df['KG_LIQUIDO']
 ex_valorFob_24 = ex24_df['VL_FOB']
 
+# Criando variáveis de importação
+imp_mes_24 = imp24_df['CO_MES']
+imp_sh4_24 = imp24_df['SH4']
+imp_estado_24 = imp24_df['SG_UF_MUN']
+imp_municipio_24 = imp24_df['CO_MUN']
+imp_liquido_24 = imp24_df['KG_LIQUIDO']
+imp_valorFob_24 = imp24_df['VL_FOB']
+
 # Criando a variável de cidade(primeiro filtro)
 cidade = "Pirapozinho"
 
 # Filtrar os dados por mês, estado, municipio e calcular o total de exportações de todos os meses
-janeiro24 = ex24_df.loc[ex_mes_24 == 1]
-janeiro24 = janeiro24[janeiro24["SG_UF_MUN"] == "SP"]
-janeiro24 = janeiro24[janeiro24["NO_MUN_MIN"] == cidade]
-janeiro24_total = janeiro24["KG_LIQUIDO"].sum()
+ex_janeiro24 = ex24_df.loc[ex_mes_24 == 1]
+ex_janeiro24 = ex_janeiro24[ex_janeiro24["SG_UF_MUN"] == "SP"]
+ex_janeiro24 = ex_janeiro24[ex_janeiro24["NO_MUN_MIN"] == cidade]
+ex_janeiro24_total = ex_janeiro24["KG_LIQUIDO"].sum()
 
-fev24 = ex24_df.loc[ex_mes_24 == 2]
-fev24 = fev24[fev24["SG_UF_MUN"] == "SP"]
-fev24 = fev24[fev24["NO_MUN_MIN"] == cidade]
-fev24_total = fev24["KG_LIQUIDO"].sum()
+ex_fev24 = ex24_df.loc[ex_mes_24 == 2]
+ex_fev24 = ex_fev24[ex_fev24["SG_UF_MUN"] == "SP"]
+ex_fev24 = ex_fev24[ex_fev24["NO_MUN_MIN"] == cidade]
+ex_fev24_total = ex_fev24["KG_LIQUIDO"].sum()
 
-mar24 = ex24_df.loc[ex_mes_24 == 3]
-mar24 = mar24[mar24["SG_UF_MUN"] == "SP"]
-mar24 = mar24[mar24["NO_MUN_MIN"] == cidade]
-mar24_total = mar24["KG_LIQUIDO"].sum()
+ex_mar24 = ex24_df.loc[ex_mes_24 == 3]
+ex_mar24 = ex_mar24[ex_mar24["SG_UF_MUN"] == "SP"]
+ex_mar24 = ex_mar24[ex_mar24["NO_MUN_MIN"] == cidade]
+ex_mar24_total = ex_mar24["KG_LIQUIDO"].sum()
 
-abr24 = ex24_df.loc[ex_mes_24 == 4]
-abr24 = abr24[abr24["SG_UF_MUN"] == "SP"]
-abr24 = abr24[abr24["NO_MUN_MIN"] == cidade]
-abr24_total = abr24["KG_LIQUIDO"].sum()
+ex_abr24 = ex24_df.loc[ex_mes_24 == 4]
+ex_abr24 = ex_abr24[ex_abr24["SG_UF_MUN"] == "SP"]
+ex_abr24 = ex_abr24[ex_abr24["NO_MUN_MIN"] == cidade]
+ex_abr24_total = ex_abr24["KG_LIQUIDO"].sum()
 
-mai24 = ex24_df.loc[ex_mes_24 == 5]
-mai24 = mai24[mai24["SG_UF_MUN"] == "SP"]
-mai24 = mai24[mai24["NO_MUN_MIN"] == cidade]
-mai24_total = mai24["KG_LIQUIDO"].sum()
+ex_mai24 = ex24_df.loc[ex_mes_24 == 5]
+ex_mai24 = ex_mai24[ex_mai24["SG_UF_MUN"] == "SP"]
+ex_mai24 = ex_mai24[ex_mai24["NO_MUN_MIN"] == cidade]
+ex_mai24_total = ex_mai24["KG_LIQUIDO"].sum()
 
-jun24 = ex24_df.loc[ex_mes_24 == 6]
-jun24 = jun24[jun24["SG_UF_MUN"] == "SP"]
-jun24 = jun24[jun24["NO_MUN_MIN"] == cidade]
-jun24_total = jun24["KG_LIQUIDO"].sum()
+ex_jun24 = ex24_df.loc[ex_mes_24 == 6]
+ex_jun24 = ex_jun24[ex_jun24["SG_UF_MUN"] == "SP"]
+ex_jun24 = ex_jun24[ex_jun24["NO_MUN_MIN"] == cidade]
+ex_jun24_total = ex_jun24["KG_LIQUIDO"].sum()
 
-jul24 = ex24_df.loc[ex_mes_24 == 7]
-jul24 = jul24[jul24["SG_UF_MUN"] == "SP"]
-jul24 = jul24[jul24["NO_MUN_MIN"] == cidade]
-jul24_total = jul24["KG_LIQUIDO"].sum()
+ex_jul24 = ex24_df.loc[ex_mes_24 == 7]
+ex_jul24 = ex_jul24[ex_jul24["SG_UF_MUN"] == "SP"]
+ex_jul24 = ex_jul24[ex_jul24["NO_MUN_MIN"] == cidade]
+ex_jul24_total = ex_jul24["KG_LIQUIDO"].sum()
 
-ago24 = ex24_df.loc[ex_mes_24 == 8]
-ago24 = ago24[ago24["SG_UF_MUN"] == "SP"]
-ago24 = ago24[ago24["NO_MUN_MIN"] == cidade]
-ago24_total = ago24["KG_LIQUIDO"].sum()
+ex_ago24 = ex24_df.loc[ex_mes_24 == 8]
+ex_ago24 = ex_ago24[ex_ago24["SG_UF_MUN"] == "SP"]
+ex_ago24 = ex_ago24[ex_ago24["NO_MUN_MIN"] == cidade]
+ex_ago24_total = ex_ago24["KG_LIQUIDO"].sum()
 
-set24 = ex24_df.loc[ex_mes_24 == 9]
-set24 = set24[set24["SG_UF_MUN"] == "SP"]
-set24 = set24[set24["NO_MUN_MIN"] == cidade]
-set24_total = set24["KG_LIQUIDO"].sum()
+ex_set24 = ex24_df.loc[ex_mes_24 == 9]
+ex_set24 = ex_set24[ex_set24["SG_UF_MUN"] == "SP"]
+ex_set24 = ex_set24[ex_set24["NO_MUN_MIN"] == cidade]
+ex_set24_total = ex_set24["KG_LIQUIDO"].sum()
 
-out24 = ex24_df.loc[ex_mes_24 == 10]
-out24 = out24[out24["SG_UF_MUN"] == "SP"]
-out24 = out24[out24["NO_MUN_MIN"] == cidade]
-out24_total = out24["KG_LIQUIDO"].sum()
+ex_out24 = ex24_df.loc[ex_mes_24 == 10]
+ex_out24 = ex_out24[ex_out24["SG_UF_MUN"] == "SP"]
+ex_out24 = ex_out24[ex_out24["NO_MUN_MIN"] == cidade]
+ex_out24_total = ex_out24["KG_LIQUIDO"].sum()
 
-nov24 = ex24_df.loc[ex_mes_24 == 11]
-nov24 = nov24[nov24["SG_UF_MUN"] == "SP"]
-nov24 = nov24[nov24["NO_MUN_MIN"] == cidade]
-nov24_total = nov24["KG_LIQUIDO"].sum()
+ex_nov24 = ex24_df.loc[ex_mes_24 == 11]
+ex_nov24 = ex_nov24[ex_nov24["SG_UF_MUN"] == "SP"]
+ex_nov24 = ex_nov24[ex_nov24["NO_MUN_MIN"] == cidade]
+ex_nov24_total = ex_nov24["KG_LIQUIDO"].sum()
 
-dez24 = ex24_df.loc[ex_mes_24 == 12]
-dez24 = dez24[dez24["SG_UF_MUN"] == "SP"]
-dez24 = dez24[dez24["NO_MUN_MIN"] == cidade]
-dez24_total = dez24["KG_LIQUIDO"].sum()
+ex_dez24 = ex24_df.loc[ex_mes_24 == 12]
+ex_dez24 = ex_dez24[ex_dez24["SG_UF_MUN"] == "SP"]
+ex_dez24 = ex_dez24[ex_dez24["NO_MUN_MIN"] == cidade]
+ex_dez24_total = ex_dez24["KG_LIQUIDO"].sum()
+
+# Filtrar os dados por mês, estado, municipio e calcular o total de exportações de todos os meses
+imp_janeiro24 = imp24_df.loc[imp_mes_24 == 1]
+imp_janeiro24 = imp_janeiro24[imp_janeiro24["SG_UF_MUN"] == "SP"]
+imp_janeiro24 = imp_janeiro24[imp_janeiro24["NO_MUN_MIN"] == cidade]
+imp_janeiro24_total = imp_janeiro24["KG_LIQUIDO"].sum()
+
+imp_fev24 = imp24_df.loc[imp_mes_24 == 2]
+imp_fev24 = imp_fev24[imp_fev24["SG_UF_MUN"] == "SP"]
+imp_fev24 = imp_fev24[imp_fev24["NO_MUN_MIN"] == cidade]
+imp_fev24_total = imp_fev24["KG_LIQUIDO"].sum()
+
+imp_mar24 = imp24_df.loc[imp_mes_24 == 3]
+imp_mar24 = imp_mar24[imp_mar24["SG_UF_MUN"] == "SP"]
+imp_mar24 = imp_mar24[imp_mar24["NO_MUN_MIN"] == cidade]
+imp_mar24_total = imp_mar24["KG_LIQUIDO"].sum()
+
+imp_abr24 = imp24_df.loc[imp_mes_24 == 4]
+imp_abr24 = imp_abr24[imp_abr24["SG_UF_MUN"] == "SP"]
+imp_abr24 = imp_abr24[imp_abr24["NO_MUN_MIN"] == cidade]
+imp_abr24_total = imp_abr24["KG_LIQUIDO"].sum()
+
+imp_mai24 = imp24_df.loc[imp_mes_24 == 5]
+imp_mai24 = imp_mai24[imp_mai24["SG_UF_MUN"] == "SP"]
+imp_mai24 = imp_mai24[imp_mai24["NO_MUN_MIN"] == cidade]
+imp_mai24_total = imp_mai24["KG_LIQUIDO"].sum()
+
+imp_jun24 = imp24_df.loc[imp_mes_24 == 6]
+imp_jun24 = imp_jun24[imp_jun24["SG_UF_MUN"] == "SP"]
+imp_jun24 = imp_jun24[imp_jun24["NO_MUN_MIN"] == cidade]
+imp_jun24_total = imp_jun24["KG_LIQUIDO"].sum()
+
+imp_jul24 = imp24_df.loc[imp_mes_24 == 7]
+imp_jul24 = imp_jul24[imp_jul24["SG_UF_MUN"] == "SP"]
+imp_jul24 = imp_jul24[imp_jul24["NO_MUN_MIN"] == cidade]
+imp_jul24_total = imp_jul24["KG_LIQUIDO"].sum()
+
+imp_ago24 = imp24_df.loc[imp_mes_24 == 8]
+imp_ago24 = imp_ago24[imp_ago24["SG_UF_MUN"] == "SP"]
+imp_ago24 = imp_ago24[imp_ago24["NO_MUN_MIN"] == cidade]
+imp_ago24_total = imp_ago24["KG_LIQUIDO"].sum()
+
+imp_set24 = imp24_df.loc[imp_mes_24 == 9]
+imp_set24 = imp_set24[imp_set24["SG_UF_MUN"] == "SP"]
+imp_set24 = imp_set24[imp_set24["NO_MUN_MIN"] == cidade]
+imp_set24_total = imp_set24["KG_LIQUIDO"].sum()
+
+imp_out24 = imp24_df.loc[imp_mes_24 == 10]
+imp_out24 = imp_out24[imp_out24["SG_UF_MUN"] == "SP"]
+imp_out24 = imp_out24[imp_out24["NO_MUN_MIN"] == cidade]
+imp_out24_total = imp_out24["KG_LIQUIDO"].sum()
+
+imp_nov24 = imp24_df.loc[imp_mes_24 == 11]
+imp_nov24 = imp_nov24[imp_nov24["SG_UF_MUN"] == "SP"]
+imp_nov24 = imp_nov24[imp_nov24["NO_MUN_MIN"] == cidade]
+imp_nov24_total = imp_nov24["KG_LIQUIDO"].sum()
+
+imp_dez24 = imp24_df.loc[imp_mes_24 == 12]
+imp_dez24 = imp_dez24[imp_dez24["SG_UF_MUN"] == "SP"]
+imp_dez24 = imp_dez24[imp_dez24["NO_MUN_MIN"] == cidade]
+imp_dez24_total = imp_dez24["KG_LIQUIDO"].sum()
 
 #
 # Parte dos gráficos para colocar os valores da tabela
@@ -98,7 +168,7 @@ from scipy.interpolate import make_interp_spline
 # Definir os meses e valores fictícios
 meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 x = np.arange(len(meses))
-valores = [janeiro24_total, fev24_total, mar24_total, abr24_total, mai24_total, jun24_total, jul24_total, ago24_total, set24_total, out24_total, nov24_total, dez24_total]
+valores = [ex_janeiro24_total, ex_fev24_total, ex_mar24_total, ex_abr24_total, ex_mai24_total, ex_jun24_total, ex_jul24_total, ex_ago24_total, ex_set24_total, ex_out24_total, ex_nov24_total, ex_dez24_total]
 
 # Criar gráfico interativo
 fig = go.Figure()
